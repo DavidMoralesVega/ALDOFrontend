bb6274cefbaf5d4fdf96faf0487132cdba76ed46
RobertoUgarte <robertougarteguti@gmail.com>
2022-12-07 09:33:54 -0400

    finished HU-4

--- src/app/modules/admin/pages/request-written/components/request-written-update/container/request-written-update.component.ts
+++ src/app/modules/admin/pages/request-written/components/request-written-update/container/request-written-update.component.ts
@@ -1,15 +1,98 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Observable } from 'rxjs';
+import { Pagination } from 'src/app/core/entities';
+import { DefaultErrorMatcher } from 'src/app/core/shared/default.error-matcher';
+import { RequestWrittenAdapter, UpdateRequestWrittenDto } from '../../../entities';
+import { RequestWrittenFacade } from '../../../facades/request-written.facade';
 
 @Component({
-  selector: 'z-request-written-update',
-  templateUrl: './request-written-update.component.html',
-  styleUrls: ['./request-written-update.component.scss']
+	selector: 'z-request-written-update',
+	templateUrl: './request-written-update.component.html',
+	styleUrls: ['./request-written-update.component.scss']
 })
 export class RequestWrittenUpdateComponent implements OnInit {
+	public readonly errorMatcher: DefaultErrorMatcher = new DefaultErrorMatcher();
+	public formUpdate: FormGroup = new FormGroup({});
+	public updateIsLoading$: Observable<boolean>;
 
-  constructor() { }
+	private pagination: Pagination = {
+		limit: 100,
+		offset: 0,
+		filter: 'ALL'
+	};
 
-  ngOnInit(): void {
-  }
+	constructor(
+		@Inject(MAT_DIALOG_DATA)
+		private readonly requestWrittenAdapter: RequestWrittenAdapter,
+		private readonly requestWrittenFacade: RequestWrittenFacade
+	) {
+		this.updateIsLoading$ = requestWrittenFacade.updateIsLoading$;
+	}
+	ngOnInit(): void {
+		this.initFormUpdate();
+	}
+	initFormUpdate(): void {
+		this.formUpdate = new FormGroup({
+			RWTitle: new FormControl(this.requestWrittenAdapter.RWTitle, [
+				Validators.required,
+				Validators.maxLength(40)
+			]),
+			RWSummary: new FormControl(this.requestWrittenAdapter.RWSummary, [
+				Validators.required,
+				Validators.maxLength(40)
+			]),
+			RWPublicationDate: new FormControl(this.requestWrittenAdapter.RWPublicationDate, [
+				Validators.required
+			]),
+			RWIssueDate: new FormControl(this.requestWrittenAdapter.RWIssueDate, [
+				Validators.required
+			]),
+			RWDocumentNumber: new FormControl(this.requestWrittenAdapter.RWDocumentNumber, [
+				Validators.required
+			]),
+			RWVisibility: new FormControl(this.requestWrittenAdapter.RWVisibility, [
+				Validators.required
+			])
+		});
+	}
+	get RWTitle() {
+		return this.formUpdate.get('RWTitle')!;
+	}
+	get RWSummary() {
+		return this.formUpdate.get('RWSummary')!;
+	}
+	get RWPublicationDate() {
+		return this.formUpdate.get('RWPublicationDate')!;
+	}
+	get RWIssueDate() {
+		return this.formUpdate.get('RWIssueDate')!;
+	}
+	get RWDocumentNumber() {
+		return this.formUpdate.get('RWDocumentNumber')!;
+	}
+	get RWVisibility() {
+		return this.formUpdate.get('RWVisibility')!;
+	}
+	update() {
+		if (this.formUpdate.invalid) return;
 
+		const updateRequestWrittenDto: UpdateRequestWrittenDto = {
+			RWTitle: this.RWTitle.value,
+			RWSummary: this.RWSummary.value,
+			RWPublicationDate: this.RWPublicationDate.value,
+			RWIssueDate: this.RWIssueDate.value,
+			RWDocumentNumber: this.RWDocumentNumber.value,
+			RWVisibility: this.RWVisibility.value
+		};
+		console.log(this.requestWrittenAdapter.IdRequestWritten);
+
+		this.requestWrittenFacade.update(
+			this.requestWrittenAdapter.IdRequestWritten,
+			updateRequestWrittenDto
+		);
+
+		// this.matDialogRef.close();
+	}
 }
--- src/app/modules/admin/pages/request-written/components/request-written-update/container/request-written-update/request-written-update.module.ts
+++ src/app/modules/admin/pages/request-written/components/request-written-update/container/request-written-update/request-written-update.module.ts
@@ -1,15 +1,34 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RequestWrittenUpdateComponent } from '../request-written-update.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
+import { MatRadioModule } from '@angular/material/radio';
 
-
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
 
 @NgModule({
-  declarations: [
-    RequestWrittenUpdateComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+	declarations: [RequestWrittenUpdateComponent],
+	imports: [
+		CommonModule,
+		ReactiveFormsModule,
+		MatDialogModule,
+		MatFormFieldModule,
+		MatInputModule,
+		MatIconModule,
+		MatButtonModule,
+		MatSelectModule,
+		MatRadioModule,
+		//fecha
+		MatDatepickerModule,
+		MatNativeDateModule
+	],
+	exports: [RequestWrittenUpdateComponent]
 })
-export class RequestWrittenUpdateModule { }
+export class RequestWrittenUpdateModule {}
