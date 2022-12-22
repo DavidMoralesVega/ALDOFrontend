d7eb606d06151b59e0c3c01a81a291a845f8f854
DavidMV <moralesvegadavid@gmail.com>
2022-12-21 23:30:33 -0400

    hotfix

--- src/app/modules/public/pages/commission/commission.module.ts
+++ src/app/modules/public/pages/commission/commission.module.ts
@@ -3,15 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { CommissionRoutingModule } from './commission-routing.module';
 import { CommissionComponent } from './container/commission.component';
-
+import { NgOptimizedImage } from '@angular/common';
 
 @NgModule({
-  declarations: [
-    CommissionComponent
-  ],
-  imports: [
-    CommonModule,
-    CommissionRoutingModule
-  ]
+	declarations: [CommissionComponent],
+	imports: [CommonModule, CommissionRoutingModule, NgOptimizedImage]
 })
-export class CommissionModule { }
+export class CommissionModule {}
